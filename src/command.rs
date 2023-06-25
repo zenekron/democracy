05516d5bfb264993c2ea4d2bc6addc0e99c68bd7
zenekron <zenekron@gmail.com>
2023-06-25 15:24:57 +0200

    feat(bot): create the `/invite` command

--- src/command.rs
+++ src/command.rs
@@ -1,10 +1,12 @@
+use std::str::FromStr;
+
 use serenity::{
-    futures::future::join_all,
     model::prelude::{
-        command::Command as SerenityCommand,
+        command::{Command as SerenityCommand, CommandOptionType},
         interaction::{
             application_command::ApplicationCommandInteraction, InteractionResponseType,
         },
+        ReactionType, UserId,
     },
     prelude::Context,
 };
@@ -14,19 +16,27 @@ use crate::error::Error;
 #[derive(Debug)]
 pub enum Command {
     Ping,
+    Invite { user_id: UserId },
 }
 
 impl Command {
     pub async fn register(ctx: Context) -> Result<(), Error> {
-        let commands = [SerenityCommand::create_global_application_command(
-            &ctx.http,
-            |cmd| cmd.name("ping").description("Ping"),
-        )];
+        let _ping = SerenityCommand::create_global_application_command(&ctx.http, |cmd| {
+            cmd.name("ping").description("Ping")
+        })
+        .await?;
 
-        let _commands = join_all(commands)
-            .await
-            .into_iter()
-            .collect::<Result<Vec<_>, _>>()?;
+        let _invite = SerenityCommand::create_global_application_command(&ctx.http, |cmd| {
+            cmd.name("invite")
+                .description("Creates a petition to invite a new user")
+                .create_option(|opt| {
+                    opt.name("user_id")
+                        .kind(CommandOptionType::String)
+                        .description("The ID of the user to invite")
+                        .required(true)
+                })
+        })
+        .await?;
 
         Ok(())
     }
@@ -49,6 +59,49 @@ impl Command {
 
                 Ok(())
             }
+
+            Command::Invite { user_id } => {
+                let user = user_id.to_user(&ctx.http).await?;
+                debug!("user: {:?}", user);
+
+                command
+                    .create_interaction_response(&ctx.http, |resp| {
+                        resp.kind(InteractionResponseType::ChannelMessageWithSource)
+                            .interaction_response_data(|data| {
+                                data.embed(|embed| {
+                                    embed
+                                        .color(colors::PASTEL_GREEN)
+                                        .title("Invite Poll")
+                                        .thumbnail(user.face())
+                                        .field("User", &user.name, true)
+                                })
+                                .components(|component| {
+                                    component.create_action_row(|row| {
+                                        row.create_button(|btn| {
+                                            btn.custom_id("invite.vote.yes")
+                                                .label("Yes")
+                                                .emoji(ReactionType::from('🟢'))
+                                        })
+                                        .create_button(|btn| {
+                                            btn.custom_id("invite.vote.maybe")
+                                                .label("Maybe")
+                                                .emoji(ReactionType::from('🟡'))
+                                        })
+                                        .create_button(
+                                            |btn| {
+                                                btn.custom_id("invite.vote.no")
+                                                    .label("No")
+                                                    .emoji(ReactionType::from('🔴'))
+                                            },
+                                        )
+                                    })
+                                })
+                            })
+                    })
+                    .await?;
+
+                Ok(())
+            }
         }
     }
 }
@@ -59,7 +112,46 @@ impl TryFrom<&ApplicationCommandInteraction> for Command {
     fn try_from(value: &ApplicationCommandInteraction) -> Result<Self, Self::Error> {
         match value.data.name.as_str() {
             "ping" => Ok(Command::Ping),
+            "invite" => {
+                let mut user_id: Option<UserId> = None;
+
+                for opt in &value.data.options {
+                    match opt.name.as_str() {
+                        "user_id" => {
+                            user_id = opt
+                                .value
+                                .as_ref()
+                                .and_then(|val| val.as_str())
+                                .map(FromStr::from_str)
+                                .transpose()?;
+                        }
+
+                        other => {
+                            return Err(Error::UnknownCommandOption(
+                                "invite".to_owned(),
+                                other.to_owned(),
+                            ))
+                        }
+                    }
+                }
+
+                match user_id {
+                    Some(user_id) => Ok(Self::Invite { user_id }),
+                    None => Err(Error::MissingCommandOption(
+                        "invite".to_owned(),
+                        "user_id".to_owned(),
+                    )),
+                }
+            }
+
             other => Err(Error::UnknownCommand(other.to_owned())),
         }
     }
+}
+
+mod colors {
+    use serenity::utils::Color;
+
+    // https://www.colorhexa.com/77dd77
+    pub static PASTEL_GREEN: Color = Color::new(0x77dd77);
 }
--- src/error.rs
+++ src/error.rs
@@ -1,8 +1,22 @@
+use serenity::model::prelude::UserIdParseError;
+
 #[derive(Debug, thiserror::Error)]
 pub enum Error {
     #[error("unknown command `{0}`")]
     UnknownCommand(String),
 
+    #[error("unknown option `{1}` for command `{0}`")]
+    UnknownCommandOption(String, String),
+
+    #[error("missing option `{1}` for command `{0}`")]
+    MissingCommandOption(String, String),
+
+    //
+    // serenity
+    //
     #[error(transparent)]
     SerenityError(#[from] serenity::Error),
+
+    #[error(transparent)]
+    UserIdParseError(#[from] UserIdParseError),
 }

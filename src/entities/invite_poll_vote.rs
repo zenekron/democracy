f2ac693cb5b608b876e9b5632854cb3e30033cf4
zenekron <zenekron@gmail.com>
2023-07-02 12:12:13 +0200

    refactor(bot,entities): declare enums in the module root

--- src/entities/invite_poll_vote.rs
+++
@@ -1,7 +0,0 @@
-#[derive(Clone, Copy, Debug, sqlx::Type)]
-#[sqlx(type_name = "invite_poll_vote", rename_all = "lowercase")]
-pub enum InvitePollVote {
-    Yes,
-    Maybe,
-    No,
-}
--- src/entities/mod.rs
+++ src/entities/mod.rs
@@ -1,9 +1,15 @@
 mod invite_poll;
-mod invite_poll_vote;
 mod invite_poll_vote_count;
 mod invite_poll_vote_submission;
 
 pub use invite_poll::InvitePoll;
-pub use invite_poll_vote::InvitePollVote;
 pub use invite_poll_vote_count::InvitePollVoteCount;
 pub use invite_poll_vote_submission::InvitePollVoteSubmission;
+
+#[derive(Clone, Copy, Debug, sqlx::Type)]
+#[sqlx(type_name = "invite_poll_vote", rename_all = "lowercase")]
+pub enum InvitePollVote {
+    Yes,
+    Maybe,
+    No,
+}

ca3563ebf9e97a69313a925d1c4cea395e2ab28b
zenekron <zenekron@gmail.com>
2023-07-02 10:48:18 +0200

    chore(migrations): configure filetype to `pgsql`

--- migrations/20230630081955_create_invite_poll_table.sql
+++ migrations/20230630081955_create_invite_poll_table.sql
@@ -1,11 +1,16 @@
+-- vim: ft=pgsql
+
 -- Trigger function that updates the `updated_at` attribute of a record to the
 -- current time.
-CREATE FUNCTION update_updated_at() RETURNS trigger AS $BODY$
+CREATE FUNCTION update_updated_at()
+RETURNS trigger
+LANGUAGE plpgsql
+AS $$
 	BEGIN
 		NEW.updated_at = now();
 		RETURN NEW;
 	END;
-$BODY$ LANGUAGE plpgsql;
+$$;
 
 
 CREATE TABLE invite_poll (

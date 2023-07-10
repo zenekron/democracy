90fccf7984e598bbf2f690340ec00eda18bdeaef
zenekron <zenekron@gmail.com>
2023-07-10 09:38:57 +0200

    chore(scripts,pre-release): use cargo-release's environment variables

--- scripts/pre-release.sh
+++ scripts/pre-release.sh
@@ -1,24 +1,55 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-usage() {
-	echo "Usage: $0 <crate_name> <version>" >&2
-	exit 1
+# https://github.com/crate-ci/cargo-release/blob/master/docs/reference.md#hook-environment-variables
+# - PREV_VERSION: The version before cargo-release was executed (before any version bump).
+# - PREV_METADATA: The version's metadata field before cargo-release was executed (before any version bump).
+# - NEW_VERSION: The current (bumped) crate version.
+# - NEW_METADATA: The current (bumped) crate version's metadata field.
+# - DRY_RUN: Whether the release is actually happening (true / false)
+# - CRATE_NAME: The name of the crate.
+# - WORKSPACE_ROOT: The path to the workspace.
+# - CRATE_ROOT: The path to the crate.
+
+
+
+#
+# util
+#
+
+run() {
+	if [[ "$DRY_RUN" = "true" ]]; then
+		echo "[skipped] $*" >&2
+	else
+		"$@"
+	fi
 }
 
-crate_name="$1"; shift
-if [[ -z "$crate_name" ]]; then usage; fi
-version="$1"; shift
-if [[ -z "$version" ]]; then usage; fi
 
-# build the container image
-docker buildx build --tag "$crate_name:$version" .
-if [[ -n "$DEMOCRACY_REGISTRY" ]]; then
-	docker image tag "$crate_name:$version" "$DEMOCRACY_REGISTRY/$crate_name:$version"
-	docker image push "$DEMOCRACY_REGISTRY/$crate_name:$version"
-fi
 
-# generate the changelog
-git cliff --output "CHANGELOG.md" --tag "$version"
-git add "CHANGELOG.md"
-git commit -m "chore(changelog): generate changelog for version $version"
+#
+# steps
+#
+
+build_image() {
+	local tag="$CRATE_NAME:$NEW_VERSION"
+	run docker builx build --tag "$tag" "$WORKSPACE_ROOT"
+
+	if [[ -n "$DEMOCRACY_REGISTRY" ]]; then
+		run docker image tag "$tag" "$DEMOCRACY_REGISTRY/$tag"
+		run docker image push "$DEMOCRACY_REGISTRY/$tag"
+	fi
+}
+
+generate_changelog() {
+	run git cliff --output "CHANGELOG.md" --tag "$NEW_VERSION"
+}
+
+
+
+#
+# main
+#
+
+build_image
+generate_changelog

0bb17f8c97406c9e664fbe496957a9070b830b2c
zenekron <zenekron@gmail.com>
2023-07-30 09:31:35 +0200

    chore: configure tracing with the opentelemetry exporter

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,15 @@
 extern crate log;
 
 use handler::Handler;
+use opentelemetry::{
+    sdk::{trace, Resource},
+    KeyValue,
+};
 use serenity::{prelude::GatewayIntents, Client};
 use settings::Settings;
 use sqlx::{postgres::PgPoolOptions, PgPool};
 use tokio::sync::OnceCell;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt, EnvFilter, Registry};
 
 mod action;
 mod background_poll_handler;
@@ -19,7 +24,23 @@ pub static POOL: OnceCell<PgPool> = OnceCell::const_new();
 
 #[tokio::main]
 async fn main() -> Result<(), crate::error::Error> {
-    tracing_subscriber::fmt::init();
+    // opentelemetry
+    let tracer = opentelemetry_otlp::new_pipeline()
+        .tracing()
+        .with_exporter(opentelemetry_otlp::new_exporter().tonic())
+        .with_trace_config(trace::config().with_resource(Resource::new(vec![
+            KeyValue::new("service.name", env!("CARGO_BIN_NAME")),
+            KeyValue::new("service.version", env!("CARGO_PKG_VERSION")),
+        ])))
+        .install_batch(opentelemetry::runtime::Tokio)
+        .unwrap();
+
+    // tracing
+    Registry::default()
+        .with(tracing_opentelemetry::layer().with_tracer(tracer))
+        .with(EnvFilter::from_default_env())
+        .with(tracing_subscriber::fmt::layer())
+        .init();
 
     let config = Settings::try_load()?;
 
